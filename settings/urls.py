bcb92d12e57ef514a697fe3c5b9de003df063d49
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2024-11-30 14:18:49 +0500

    create auth view, join redoc&swagger

--- settings/urls.py
+++ settings/urls.py
@@ -1,14 +1,41 @@
 # Django
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-# RestFramework SimpleJWT
-from rest_framework_simplejwt.views import \
-    TokenObtainPairView, TokenRefreshView
+# RestFramework
+from rest_framework import permissions
 
+# Third-Party
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Local
+from auths.views import AuthView
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version="v1",
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("admin/", admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/v1/auth/", AuthView.as_view())
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls"))
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
